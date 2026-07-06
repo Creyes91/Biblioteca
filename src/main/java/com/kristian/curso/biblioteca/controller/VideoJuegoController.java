ac76b906fd265e16d4ccc4058c492bf059a6d594
Cristian <cd.reyes@icloud.com>
2026-07-06 16:09:04 +0200

    feat: Añadir formulario de registro de videojuegos

--- src/main/java/com/kristian/curso/biblioteca/controller/VideoJuegoController.java
+++ src/main/java/com/kristian/curso/biblioteca/controller/VideoJuegoController.java
@@ -1,7 +1,12 @@
 package com.kristian.curso.biblioteca.controller;
 
+import com.kristian.curso.biblioteca.model.VideoJuego;
+import com.kristian.curso.biblioteca.service.IGeneroService;
+import com.kristian.curso.biblioteca.service.IPlataformaService;
 import com.kristian.curso.biblioteca.service.VideoJuegoServiceImpl;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -9,12 +14,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/juegos")
 public class VideoJuegoController {
 
+    private IPlataformaService plataformaService;
+    private IGeneroService generoService;
+
     private VideoJuegoServiceImpl juegoService;
-    public VideoJuegoController(VideoJuegoServiceImpl juegoService)
+    public VideoJuegoController(VideoJuegoServiceImpl juegoService, IPlataformaService plataformaService, IGeneroService generoService)
     {
         this.juegoService = juegoService;
+        this.plataformaService = plataformaService;
+        this.generoService = generoService;
     }
 
+
+    @GetMapping("/nuevo")
+    public String nuevoJuego(Model model) {
+        model.addAttribute("plataformas", plataformaService.findAll());
+        model.addAttribute("generos", generoService.findAll());
+        return "VideoJuegosForm";
+    }
 
 
 
